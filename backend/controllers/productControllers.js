9296607ff86b9f27e42295221693acece9df75da
Glauber Castro <glauber.castro@insa.gov.br>
2021-09-08 16:28:21 -0300

    Code Redux and Axios

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -20,9 +20,9 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
 //Get all products => /api/v1/products?keyword=apple
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-
-    const resPerPage = 4;
-    const productCount = await product.countDocuments()
+    
+    const resPerPage = 8;
+    const productsCount = await product.countDocuments()
 
     const apiFeatures = new APIFeatures(Product.find(), req.query)
         .search()
@@ -31,12 +31,12 @@ exports.getProducts = catchAsyncErrors(async (req, res, next) => {
 
     const products = await apiFeatures.query;
 
-
     res.status(200).json({
         success: true,
-        productCount,
+        productsCount,
         products
     })
+    
 });
 
 //Get single product details => /api/v1/product/:id
